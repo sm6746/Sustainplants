5208b309bc7069c41544e2e2c8ab12b1459e8baa
sm6746 <104511597+sm6746@users.noreply.github.com>
2025-09-28 18:39:25 +0530

    Delete Downloads/linkedin-reach-dash-main/linkedin-reach-dash-main directory

--- Downloads/linkedin-reach-dash-main/linkedin-reach-dash-main/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import AppExtension from "./AppExtension.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<AppExtension />);
